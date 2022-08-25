bee07f50a35fcc3bf292fe8ea32e571cb63bec20
august <sum-rock@rockyhillhouse.org>
2022-08-25 12:48:13 -0500

    Reconcile dependent migrations

--- auditlog/migrations/0012_alter_logentry_changes.py
+++ auditlog/migrations/0012_alter_logentry_changes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("auditlog", "0010_alter_logentry_timestamp"),
+        ("auditlog", "0011_logentry_serialized_data"),
     ]
 
     operations = [
